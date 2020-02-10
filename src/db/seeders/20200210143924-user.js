b12a8037ccc3a2d712ee0618988246155300a675
eveins <maxmost512@gmail.com>
2020-02-10 18:02:37 +0200

    add seeders insert

--- src/db/seeders/20200210143924-user.js
+++ src/db/seeders/20200210143924-user.js
@@ -1,19 +1,26 @@
 'use strict';
 
+function generateUsers () {
+  const users = [];
+  for (let i = 0; i < 50;) {
+    users.push( new Object( {
+      firstName: 'Test',
+      lastName: 'Testovich',
+      login: `test_login${++i}`,
+      email: `test${i}@gmail.com`,
+      passwordHash: bcrypt.hashSync( `password${i}`, bcrypt.genSaltSync( 8 ) ),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    } ) )
+    ;
+  }
+  return users;
 
+}
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert( 'Users', generateUsers(), {} );
   },
 
   down: (queryInterface, Sequelize) => {
