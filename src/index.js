17dbb41db6e845ccf2ba857a7ac9b7c6a650594f
eveins <maxmost512@gmail.com>
2020-02-10 17:56:38 +0200

    Server initialization

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const {sequelize} = require('./db/models');
+const express = require('express');
+
+const PORT = process.env.NODE_ENV || 3000;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+
+    sequelize.authentificate().then(() => {
+        res.send('Ok')
+    }).catch(() => {
+        res.status(404).send('Not ok')
+    })
+});
+
+app.listen(PORT,
+    ()=>console.log(`Server app started on port${PORT}`));
