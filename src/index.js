63cfcf58e55f6eb9ca58c406d093705101c58762
eveins <maxmost512@gmail.com>
2020-02-14 15:34:21 +0200

    add extractUserId.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router=require('./routes');
-
 const PORT = process.env.NODE_ENV || 3000;
 
 const app = express();
 app.use(express.json());
-
 app.use(router);
-
-
 app.listen(PORT,
            () => console.log(`Server app started on port${PORT}`));
