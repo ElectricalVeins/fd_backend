20ccfa30c8562f1963b6e282b338468814f92990
eveins <maxmost512@gmail.com>
2020-02-20 15:13:37 +0200

    add: cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const errorHandlers = require('./middlewares/error_handlers');
 const router=require('./router');
 const PORT = process.env.NODE_ENV || 3000;
+var cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use(router);
 
